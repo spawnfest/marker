d207cf03fdbe3d6e3c2f4d74419a04d8d8a06422
andreyuhai <burakaymakci@gmail.com>
2021-09-18 20:11:33 +0200

    Implement render_markdown/1 to render parsed markdown

--- marker_web/lib/marker_web_web/live/page_live.ex
+++ marker_web/lib/marker_web_web/live/page_live.ex
@@ -4,16 +4,30 @@ defmodule MarkerWebWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, markdown: "", result: "")}
+    {:ok, assign(socket, markdown: "", result: "", rendered_markdown: "")}
   end
 
   @impl true
   def handle_event("to_html", %{"m" => markdown}, socket) do
     Logger.info("got event #{markdown}")
-    {:noreply, assign(socket, markdown: "", result: to_html(markdown))}
+    {:ok, parsed_markdown} = parse_markdown(markdown)
+
+    {
+      :noreply,
+      assign(
+        socket,
+        markdown: "",
+        result: parsed_markdown,
+        rendered_markdown: render_markdown(parsed_markdown)
+      )
+    }
   end
 
-  defp to_html(markdown) do
+  defp parse_markdown(markdown) do
     :marker.markdown(markdown |> to_charlist())
+  end
+
+  defp render_markdown(parsed_markdown) do
+    :render.block_to_html(parsed_markdown)
   end
 end
